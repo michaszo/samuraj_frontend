5181297cc53613baa002380844f33d013a61c66f
michaszo <michaszo.iplus@gmail.com.pl>
2020-04-08 21:49:25 +0200

    searchElements

--- js/script.js
+++ js/script.js
@@ -11,8 +11,30 @@ const addElement = (event, element_, text_, attr_, value_) => {
     document.querySelector('.content').appendChild(element);
 }
 
+const searchElements = (event, phrase) => {
+    event.preventDefault();
+    const infoElement = document.querySelector('.result');
+    const elements = document.querySelectorAll(phrase);
+    
+    if(elements.length) {
+        infoElement.innerHTML = `<h2 class="result__info">${elements.length} ${phrase} found.</h2>`;
+    } else {
+        infoElement.innerHTML = `<h2 class="result__info">No ${phrase} found.</h2>`;
+        return;
+    }
+    
+}
+
+const showInfo = () => {
+
+};
+
 const addForm = document.querySelector('.form--add');
 addForm.addEventListener('submit', (event) => {
     return addElement(event, addForm.elements.element.value, addForm.elements.text.value, addForm.elements.attr.value, addForm.elements.value.value);
 });
 
+const searchForm = document.querySelector('.form--search');
+searchForm.addEventListener('submit', (event) => {
+    return searchElements(event, searchForm.elements.phrase.value);
+});
